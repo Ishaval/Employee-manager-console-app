8d353d9232e2ddca6b707b4714fde15ce546016e
Julian Ramirez <juliannrrra@outlook.es>
2023-02-23 23:18:08 -0500

    correction in index.js

--- db/schema.sql
+++ db/schema.sql
@@ -13,21 +13,21 @@ CREATE TABLE departments (
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,2) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (department_id) 
-  REFERENCES departments (id)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) 
+    REFERENCES departments (id)
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES roles (id),
-  FOREIGN KEY (manager_id) REFERENCES employees (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employees (id)
 );
--- index.js
+++ index.js
@@ -1,106 +1,170 @@
-const express = require('express');
-const mysql = require('mysql2');
+import inquirer from "inquirer";
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+import mysql from "mysql";
+import cTable from "console.table";
 
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'resources_db',
-  password: 'Killerqueen1909*'
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Killerqueen1909*',
+  database : 'resources_db'
 });
+ 
+const menu = ([
+    
+    {
+        type: "list",
+        name: "container",
+        message: "What do you want to do?",
+        choices:[
+          "view all departments", 
+            "view all roles", 
+            "view all employees", 
+           "add a department", 
+            "add a role", 
+           "add an employee", 
+           "update an employee role"
+        ]
+    }
+    
+])
 
-// view all departments
-app.get('/departments', (req, res) => {
-    const query = 'SELECT department_id, department_name FROM departments';
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      res.send(results);
-    });
-  });
+function viewMenu() {
   
-  // view all roles
-  app.get('/roles', (req, res) => {
-    const query = 'SELECT role_id, role_title, department_name, salary FROM roles INNER JOIN departments ON roles.department_id = departments.department_id';
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      res.send(results);
-    });
-  });
-  
-  // view all employees
-  app.get('/employees', (req, res) => {
-    const query = 'SELECT e.employee_id, e.first_name, e.last_name, r.role_title, d.department_name, r.salary, CONCAT(m.first_name, " ", m.last_name) AS manager_name FROM employees e LEFT JOIN roles r ON e.role_id = r.role_id LEFT JOIN departments d ON r.department_id = d.department_id LEFT JOIN employees m ON e.manager_id = m.employee_id';
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      res.send(results);
-    });
-  });
-  
-  // add a department
-  app.post('/departments', (req, res) => {
-    const departmentName = req.body.department_name;
-    const query = `INSERT INTO departments (department_name) VALUES ('${departmentName}')`;
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      res.send('Department added successfully');
-    });
-  });
-  
-  // add a role
-  app.post('/roles', (req, res) => {
-    const roleTitle = req.body.role_title;
-    const salary = req.body.salary;
-    const departmentId = req.body.department_id;
-    const query = `INSERT INTO roles (role_title, salary, department_id) VALUES ('${roleTitle}', ${salary}, ${departmentId})`;
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-      res.send('Role added successfully');
-    });
-  });
-  
-  // add an employee
-app.post('/employees', (req, res) => {
-    const firstName = req.body.first_name;
-    const lastName = req.body.last_name;
-    const roleId = req.body.role_id;
-    const managerId = req.body.manager_id;
-    const query = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('${firstName}', '${lastName}', ${roleId}, ${managerId})`;
-  
-    connection.query(query, (err, results, fields) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-  
-      res.json({
-        status: 'success',
-        message: 'Employee added successfully'
+    inquirer.prompt(menu).then((answer) => {
+      if (answer.container == "view all departments") {
+        connection.query('SELECT * FROM departments', function (error, results, fields) {
+          if (error) throw error;
+          console.table(results);
+          viewMenu();
+        });
+      } else if (answer.container == "view all roles") {
+        connection.query('SELECT * FROM roles', function (error, results, fields) {
+          if (error) throw error;
+          console.table(results);
+          viewMenu();
+        });
+      } else if (answer.container == "view all employees") {
+        connection.query('SELECT * FROM employees', function (error, results, fields) {
+          if (error) throw error;
+          console.table(results);
+          viewMenu();
+        });
+      } else if (answer.container == "add a department") {
+        const questions = [
+          {
+            type: 'input',
+            name: 'name',
+            message: "what is the name of this new department"
+          }
+        ];
+        inquirer.prompt(questions).then((answers) => {
+          connection.query(
+            'INSERT INTO departments (name) VALUES (?)',
+            [answers.name],
+            function (error, results, fields) {
+              if(error) throw error;
+              console.log(`New department ${answers.name} added to the database!`);
+              viewMenu();
+            }
+          )
+        })
+      } else if (answer.container == "add a role") {
+        const questions = [
+
+          {
+            type: 'input',
+            name: 'title',
+            message: "What is the new role name?"
+          },
+
+          {
+            type: 'input',
+            name: 'salary',
+            message: "what is the salary for the new role?"
+          },
+
+          {
+            type: 'input',
+            name: 'department_id',
+            message: "What is the department's ID?"
+          }
+        ];
+        inquirer.prompt(questions).then((answers) => {
+          connection.query(
+            'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)',
+            [answers.title, answers.salary, answers.department_id],
+            function (error, results, fields) {
+              if (error) throw error;
+              console.log(`New role ${answers.title} added to the database!`);
+              viewMenu();
+            })
+       })
+      } else if (answer.container == "add an employee") {
+        const questions = [
+          {
+              type: 'input',
+              name: 'first_name',
+              message: "What is the employee's first name?",
+          },
+          {
+              type: 'input',
+              name: 'last_name',
+              message: "What is the employee's last name?",
+          },
+          {
+              type: 'input',
+              name: 'role_id',
+              message: "What is the employee's role ID?",
+          },
+          {
+              type: 'input',
+              name: 'manager_id',
+              message: "What is the employee's manager ID?",
+          },
+      ];
+      inquirer.prompt(questions).then((answers) => {
+          connection.query(
+              'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+              [answers.first_name, answers.last_name, answers.role_id, answers.manager_id],
+              function (error, results, fields) {
+                  if (error) throw error;
+                  console.log(`New employee ${answers.first_name} ${answers.last_name} added to the database!`);
+                  viewMenu(); 
+              });
       });
+      } else if (answer.container == "update an employee role") {
+        const questions = [
+          {
+            type: "input",
+            name: "employee_id",
+            message: "Enter the ID of the employee to update:",
+          },
+          {
+            type: "input",
+            name: "role_id",
+            message: "Enter the new role ID for the employee:",
+          },
+        ];
+      
+        inquirer.prompt(questions).then((answers) => {
+          const { employee_id, role_id } = answers;
+      
+          connection.query(
+            "UPDATE employees SET role_id = ? WHERE id = ?",
+            [role_id, employee_id],
+            (err, res) => {
+              if (err) throw err;
+              console.log(`Employee with ID ${employee_id} updated successfully.`);
+              viewMenu();
+            }
+          );
+        });
+      }
+    }).catch((error) => {
+      console.log(error);
     });
-  });
-  
+  }
+
+  connection.connect();
+  viewMenu();
